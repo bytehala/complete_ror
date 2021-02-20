3f369969f9ff2391e8a337f5c95ad6f17ebf6038
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-02-19 22:22:09 -0700

    New and create actions

--- section_3/alpha-blog/app/controllers/articles_controller.rb
+++ section_3/alpha-blog/app/controllers/articles_controller.rb
@@ -9,4 +9,16 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id]) # @ sign makes it an instance variable
     end
 
+    def new
+
+    end
+
+    def create
+        # render plain: params[:article] # just renders in a new webpage
+        @article = Article.new(params.require(:article).permit(:title, :description)) # Safety feature
+        # render plain: @article.inspect # show that it gets created
+        @article.save
+        redirect_to @article
+    end
+
 end
