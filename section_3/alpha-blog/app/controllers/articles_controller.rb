df9694863a86b4467c334c1d857e9c4d1f9e411a
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-02-19 21:28:02 -0700

    Article listing feature

--- section_3/alpha-blog/app/controllers/articles_controller.rb
+++ section_3/alpha-blog/app/controllers/articles_controller.rb
@@ -1,9 +1,12 @@
 class ArticlesController < ApplicationController
 
+    def index
+        @articles = Article.all
+    end
+
     def show
         # byebug # <-- debugger
         @article = Article.find(params[:id]) # @ sign makes it an instance variable
-
     end
 
 end
