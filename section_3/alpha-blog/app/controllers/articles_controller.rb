53224c9b30f4c526e2967f66329190988bb436dc
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-02-20 00:34:31 -0700

    Error message shown on UI

--- section_3/alpha-blog/app/controllers/articles_controller.rb
+++ section_3/alpha-blog/app/controllers/articles_controller.rb
@@ -10,15 +10,18 @@ class ArticlesController < ApplicationController
     end
 
     def new
-
+        @article = Article.new
     end
 
     def create
         # render plain: params[:article] # just renders in a new webpage
         @article = Article.new(params.require(:article).permit(:title, :description)) # Safety feature
         # render plain: @article.inspect # show that it gets created
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article # shortcut for article_path(@article) aka /article/:id
+        else
+            render 'new'
+        end
     end
 
 end
