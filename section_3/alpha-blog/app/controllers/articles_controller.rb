2ec554edca629a12ee9a3a35809f50f041fb8c2b
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-02-22 15:10:48 -0700

    Partials

--- section_3/alpha-blog/app/controllers/articles_controller.rb
+++ section_3/alpha-blog/app/controllers/articles_controller.rb
@@ -1,12 +1,12 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def index
         @articles = Article.all
     end
 
     def show
-        # byebug # <-- debugger
-        @article = Article.find(params[:id]) # @ sign makes it an instance variable
     end
 
     def new
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
 
     def create
         # render plain: params[:article] # just renders in a new webpage
-        @article = Article.new(params.require(:article).permit(:title, :description)) # Safety feature
+        @article = Article.new(article_params) # Safety feature
         # render plain: @article.inspect # show that it gets created
         if @article.save
             flash[:notice] = "Article was created successfully."
@@ -26,12 +26,10 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully."
             redirect_to @article
         else
@@ -40,9 +38,19 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path # articles_path is the Prefix under rails routes
+    end
+
+    # Everything below is private
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 
 end
