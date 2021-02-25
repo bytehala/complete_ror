0d652518a6415e67912d18dd479d49dd82f15085
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-02-25 08:46:27 -0700

    Before action, and showing user info in articles

--- section_3/alpha-blog/app/models/user.rb
+++ section_3/alpha-blog/app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 },
                       format: { with: VALID_EMAIL_REGEX }
     
+    before_save { self.email = email.downcase }
 end
