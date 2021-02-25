ebb5323420f88329ee46bb0004d379141a7ca991
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-02-24 18:30:10 -0700

    Mashrur Section 5 bootstrap

--- section_3/alpha-blog/app/controllers/articles_controller.rb
+++ section_3/alpha-blog/app/controllers/articles_controller.rb
@@ -1,56 +1,52 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-    before_action :set_article, only: [:show, :edit, :update, :destroy]
+  def show
+  end
 
-    def index
-        @articles = Article.all
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
     end
+  end
 
-    def show
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
     end
+  end
 
-    def new
-        @article = Article.new
-    end
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
 
-    def create
-        # render plain: params[:article] # just renders in a new webpage
-        @article = Article.new(article_params) # Safety feature
-        # render plain: @article.inspect # show that it gets created
-        if @article.save
-            flash[:notice] = "Article was created successfully."
-            redirect_to @article # shortcut for article_path(@article) aka /article/:id
-        else
-            render 'new'
-        end
-    end
+  private
 
-    def edit
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    def update
-        if @article.update(article_params)
-            flash[:notice] = "Article was updated successfully."
-            redirect_to @article
-        else
-            render 'edit'
-        end
-    end
-
-    def destroy
-        @article.destroy
-        redirect_to articles_path # articles_path is the Prefix under rails routes
-    end
-
-    # Everything below is private
-    private
-
-    def set_article
-        @article = Article.find(params[:id])
-    end
-
-    def article_params
-        params.require(:article).permit(:title, :description)
-    end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
 end
