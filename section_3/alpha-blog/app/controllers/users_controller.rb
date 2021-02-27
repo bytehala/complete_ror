518e36808dd9bc7da0b93bfa10ad12025e06729e
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-02-27 13:13:58 -0700

    Logged in and logged out behavior in ApplicationHelper

--- section_3/alpha-blog/app/controllers/users_controller.rb
+++ section_3/alpha-blog/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:show, :edit, :update]
+
     def new
         @user = User.new
     end
@@ -8,6 +10,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             flash[:notice] = "Welcome to Alpha Blog #{@user.username}"
+            session[:user_id] = @user.id
             redirect_to articles_path
         else
             render 'new'
@@ -15,11 +18,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "User was updated successfully."
             redirect_to @user # Shortcut for user_path(@user)
@@ -29,7 +30,6 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
@@ -41,6 +41,10 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 
 end
