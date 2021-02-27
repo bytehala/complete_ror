7959467249aac8d8de7b5d53ba912733ed98b48c
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-02-27 12:38:12 -0700

    Added login / logout behavior by using sessions

--- section_3/alpha-blog/app/controllers/sessions_controller.rb
+++ section_3/alpha-blog/app/controllers/sessions_controller.rb
@@ -4,11 +4,21 @@ class SessionsController < ApplicationController
     end
 
     def create
-        byebug
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Successfully logged in"
+            redirect_to user
+        else
+            flash.now[:alert] = "Wrong email/password combination" # now is used because we called render, displaying the page over again. without a redirect
+            render 'new'
+        end
     end
 
-    def edit
-        
+    def destroy
+        session[:user_id] = nil
+        flash[:notice] = "Logged out"
+        redirect_to root_path
     end
 
     
