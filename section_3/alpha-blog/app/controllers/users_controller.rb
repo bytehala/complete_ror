f9cc39a7b2044d538c48f4e4d6f8dab18c1e10e4
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-02-27 14:57:28 -0700

    Use helper_methods to restrict user actions on edit/update of user

--- section_3/alpha-blog/app/controllers/users_controller.rb
+++ section_3/alpha-blog/app/controllers/users_controller.rb
@@ -1,50 +1,57 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
 
-    before_action :set_user, only: [:show, :edit, :update]
+  def new
+    @user = User.new
+  end
 
-    def new
-        @user = User.new
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to Alpha Blog #{@user.username}"
+      session[:user_id] = @user.id
+      redirect_to articles_path
+    else
+      render "new"
     end
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            flash[:notice] = "Welcome to Alpha Blog #{@user.username}"
-            session[:user_id] = @user.id
-            redirect_to articles_path
-        else
-            render 'new'
-        end
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "User was updated successfully."
+      redirect_to @user # Shortcut for user_path(@user)
+    else
+      render "edit"
     end
+  end
 
-    def edit
+  def show
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit or delete your own user profile"
+      redirect_to @user
     end
-
-    def update
-        if @user.update(user_params)
-            flash[:notice] = "User was updated successfully."
-            redirect_to @user # Shortcut for user_path(@user)
-        else
-            render 'edit'
-        end
-    end
-
-    def show
-        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
-    end
-
-    def index
-        @users = User.paginate(page: params[:page], per_page: 5)
-    end
-
-    private
-
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
-
-    def set_user
-        @user = User.find(params[:id])
-    end
-
+  end
 end
