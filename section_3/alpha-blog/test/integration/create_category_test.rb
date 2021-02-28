af1e6a471d309ba3433bcf611e9332fe334324ca
Lemuel Dulfo <lemuel.dulfo@clio.com>
2021-02-27 18:48:04 -0700

    Restrict admin-only tests by adding sign_in_as in test_helper.rb

--- section_3/alpha-blog/test/integration/create_category_test.rb
+++ section_3/alpha-blog/test/integration/create_category_test.rb
@@ -1,10 +1,16 @@
-require 'test_helper'
+require "test_helper"
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  def setup
+    @admin_user = User.create(username: "johndoe", email: "test@email.com",
+                              password: "password", admin: true)
+    sign_in_as(@admin_user)
+  end
+
   test "get new category form and create category" do
     get "/categories/new"
     assert_response :success
-    assert_difference 'Category.count', 1 do
+    assert_difference "Category.count", 1 do
       post categories_path, params: { category: { name: "Sports" } }
       assert_response :redirect
     end
@@ -16,12 +22,11 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
   test "get new category form and reject invalid category submission" do
     get "/categories/new"
     assert_response :success
-    assert_no_difference 'Category.count' do
+    assert_no_difference "Category.count" do
       post categories_path, params: { category: { name: " " } }
-      
     end
     assert_match "errors", response.body
-    assert_select 'div.alert'
-    assert_select 'h4.alert-heading'
+    assert_select "div.alert"
+    assert_select "h4.alert-heading"
   end
 end
